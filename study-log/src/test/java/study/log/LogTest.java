a7703f689701756721f7ab16a5efe5f19d7e60a7
TACHIKOMAQAQ <1611890073@qq.com>
2019-12-03 14:47:19 +0800

    commit

--- study-log/src/test/java/study/log/LogTest.java
+++ study-log/src/test/java/study/log/LogTest.java
@@ -2,6 +2,9 @@ package study.log;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.spi.LocationAwareLogger;
+
+import java.net.URL;
 
 /**
  * @author ：mmzs
@@ -11,5 +14,11 @@ public class LogTest {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(LogTest.class);
         logger.info("TEST ");
+//        org.slf4j.spi.LocationAwareLogger
+        if(logger instanceof LocationAwareLogger){
+            System.out.println("11111111");
+        }
+        URL urlOfClass = LogTest.class.getClassLoader().getResource("org/slf4j/spi/LocationAwareLogger.class");
+        System.out.println(urlOfClass);
     }
 }
